1be26fd2028673493eb5e9948224000e085bd512
josephhansdiaz30@gmail.com <josephhansdiaz30@gmail.com>
2021-03-27 16:27:39 -0500

    Removed local CPM files

--- CPM_modules/FindCcache.cmake.cmake
+++
@@ -1,3 +0,0 @@
-include(/home/eliotvecklie/source/repos/libsdl2-gui/cmake/CPM_0.32.0.cmake)
-CPMAddPackage(NAME;Ccache.cmake;GITHUB_REPOSITORY;TheLartians/Ccache.cmake;VERSION;1.2)
-set(Ccache.cmake_FOUND TRUE)
