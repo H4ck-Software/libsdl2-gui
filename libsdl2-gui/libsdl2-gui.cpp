aabd8ca998ecccbc30b1e539b8312ce87a0131f2
Joseph Hans Diaz Gonzalez <eliotvonecklie@gmail.com>
2021-04-21 11:14:00 -0500

    ...

--- libsdl2-gui/libsdl2-gui.cpp
+++ libsdl2-gui/libsdl2-gui.cpp
@@ -3,14 +3,14 @@
 int GuiInitSDL()
 {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS) < 0) {
-		std::cerr << "[libsdl2-gui/libsdl2-gui.cpp:46] ERROR SDL could not initialize!\n\tSDL Error: " << SDL_GetError() << std::endl;
+		std::cerr << "[libsdl2-gui/libsdl2-gui.cpp:6] ERROR SDL could not initialize!\n\tSDL Error: " << SDL_GetError() << std::endl;
 		return -1;
 	}
 
    //Initialize SDL_ttf
     if(TTF_Init() == -1)
     {
-        std::cerr << "[libsdl2-gui/libsdl2-gui.cpp:53] ERROR SDL_ttf could not initialize! SDL_ttf Error: " << TTF_GetError() << std::endl;
+        std::cerr << "[libsdl2-gui/libsdl2-gui.cpp:13] ERROR SDL_ttf could not initialize! SDL_ttf Error: " << TTF_GetError() << std::endl;
         return -2;
     }
 
