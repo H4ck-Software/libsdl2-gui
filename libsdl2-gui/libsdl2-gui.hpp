f114c23a479246e236ce26e768d34ef4f10c55ef
Joseph Hans Diaz Gonzalez <eliotvonecklie@gmail.com>
2021-04-21 11:13:30 -0500

    ...

--- libsdl2-gui/libsdl2-gui.hpp
+++ libsdl2-gui/libsdl2-gui.hpp
@@ -3,25 +3,7 @@
 #include <iostream>
 #include <string>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL2_gfxPrimitives.h>
 #include <SDL2/SDL_ttf.h>
-
-#include <libsdl2-gui/transform.hpp>
-
-extern SDL_Event GuiSDLEvent;
-
-extern SDL_Renderer* GuiRenderer;
-extern SDL_Window* GuiWindow;
-extern SDL_Surface* GuiWindowSurface;
-extern TTF_Font* GuiFont;
-
-/**
- * \brief Enum for gui statuses.
- */
-enum GuiStatus {
-    GUI_EVENT_NOTHING = 0,
-    GUI_EVENT_QUIT,
-};
 
 /**
  * \brief Initialize SDL.
@@ -31,10 +13,3 @@ enum GuiStatus {
 extern int GuiInitSDL();
 
 extern int GuiCloseSDL();
-
-/**
- * \brief Handle SDL Events.
- * 
- * \return int status (A cast from a GuiStatus variable into an int, used to determine the result of the event handling)
- */
-extern int GuiEventHandler();
